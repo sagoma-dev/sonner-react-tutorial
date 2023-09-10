0b0b2ea09931d031fba8c1a7aa235ef98b4b5cc1
Samuel Gómez <sagomadev@gmail.com>
2023-09-10 21:45:10 +0200

    Add library and basic example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Toaster, toast } from "sonner";
+
 function App() {
-  return <h1>Inicio</h1>;
+  return (
+    <main>
+      <h1>Ejemplos de Toast:</h1>
+      <ul>
+        <li onClick={() => toast("Mi toast básico")}>Básico</li>
+      </ul>
+      <Toaster />
+    </main>
+  );
 }
 
 export default App;
