8082cd6aa83a3562d98d3b7a684f08ccaddf9cb6
Samuel Gómez <sagomadev@gmail.com>
2023-09-11 18:42:15 +0200

    Add example with description and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster, toast } from "sonner";
+import { BiCheck } from "react-icons/bi";
 
 function App() {
   return (
@@ -6,6 +7,16 @@ function App() {
       <h1>Ejemplos de Toast:</h1>
       <ul>
         <li onClick={() => toast("Mi toast básico")}>Básico</li>
+        <li
+          onClick={() =>
+            toast("Mi toast", {
+              description: "Con una descripcion y un icono",
+              icon: <BiCheck />,
+            })
+          }
+        >
+          Descripción e Icono
+        </li>
       </ul>
       <Toaster />
     </main>
